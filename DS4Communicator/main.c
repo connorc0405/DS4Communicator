a3b7ceac90cef0952e12c527c5c1a3ca34cc8b60
Connor Cumming <connorc0405@gmail.com>
2017-10-29 21:00:55 -0400

    Organized input and output reports into functions.

--- DS4Communicator/main.c
+++ DS4Communicator/main.c
@@ -15,7 +15,8 @@
 #define PRODUCT_ID 0x05C4
 #define VENDOR_ID 0x054C
 
-void printInputReport(unsigned char* data);
+void printInputReport(hid_device *DS4Controller);
+void writeOutputReport(hid_device *DS4Controller);
 
 const char* dpad[] = {
     [0] = "N",
@@ -30,70 +31,52 @@ const char* dpad[] = {
 };
 
 int main(int argc, char* argv[]) {
-    int res;
-    unsigned char inputReportBuf[15];
-    unsigned char outputReportBuf[78];
     
     hid_init();
     hid_device *DS4Controller = hid_open(VENDOR_ID, PRODUCT_ID, NULL);
     
-    // Input Report
-    //res = hid_read(DS4Controller, inputReportBuf, 15);
-    //printInputReport(inputReportBuf);
+    //printInputReport(DS4Controller);
     
-    
-    // Create array with first byte at front (75 bytes)
-    // Generate crc32 (4 bytes) 4 bytes can be in decimal representation
-    // Create array with crc32 at end (reversed) and no first byte (78 bytes)
-    
-    // Output Report (red led)
-    unsigned char newbuf[] = {0xa2, 0x11, 0xc0, 0x20, 0xf3, 0x04, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x43, 0x43, 0x00, 0x4d, 0x85, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0, 0, 0}; //, 0xf6, 0xbe, 0xae, 0x2f };
-    
-    uint32_t crc32 = crc_32(newbuf, 75);
-    printf("CRC32: %u\n", crc32);
-    
-    //PLAN: BITWISE OPERATORS TO CONVERT DECIMAL TO BYTE ARRAY
-    
-    printf("%d\n", crc32 >> 24); //4th byte
-    printf("%d\n", crc32 >> 16 & 0xFF); // 3rd byte
-    printf("%d\n", crc32 >> 8 & 0xFF); // 2nd byte
-    printf("%d\n", crc32 & 0xFF); // 1st byte
-    
-    // move bytes back by 1
-    for (int i = 0; i < sizeof(newbuf); i++) {
-        newbuf[i] = newbuf[i+1];
-    }
-    
-    newbuf[74] = crc32 & 0xFF;
-    newbuf[75] = crc32 >> 8 & 0xFF;
-    newbuf[76] = crc32 >> 16 & 0xFF;
-    newbuf[77] = crc32 >> 24;
-    
-    printf("%d %d %d %d", newbuf[73], newbuf[74], newbuf[75], newbuf[76]);
-
-    res = hid_write(DS4Controller, newbuf, 78);
-    //printf("Result: %d\n", res);
-    
-    return 0;
+    writeOutputReport(DS4Controller);
 }
 
-void printInputReport(unsigned char* report) {
-    printf("Left Stick: (%d, %d)\n", report[1], report[2]);
-    printf("Right Stick: (%d, %d)\n", report[3], report[4]);
-    printf("DPad: %s\n", dpad[report[5] & 15]);
-    printf("Square: %d\n", (report[5] >> 4) & 1);
-    printf("X: %d\n", (report[5] >> 5) & 1);
-    printf("Circle: %d\n", (report[5] >> 6) & 1);
-    printf("Triangle: %d\n", (report[5] >> 7) & 1);
-    printf("L1: %d\n", report[6] & 1);
-    printf("Left Trigger: %d%%\n", (report[8]*100)/255);
-    printf("R1: %d\n", (report[6] >> 1) & 1);
-    printf("Right Trigger: %d%%\n", (report[9]*100)/255);
-    printf("Share: %d\n", (report[6] >> 4) & 1);
-    printf("Options: %d\n", (report[6] >> 5) & 1);
-    printf("L3: %d\n", (report[6] >> 6) & 1);
-    printf("R3: %d\n", (report[6] >> 7) & 1);
-    printf("PS: %d\n", report[7] & 1);
-    printf("TPAD: %d\n", (report[7] >> 1) & 1);
+void writeOutputReport(hid_device *DS4Controller) {
+    
+    unsigned char outputReportBuf[] = { 0xa2, 0x11, 0xc0, 0x20, 0xf3, 0x04, 0x00, 0x00, 0x00, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x43, 0x43, 0x00, 0x4d, 0x85, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0, 0, 0 };
+    
+    uint32_t crc32 = crc_32(outputReportBuf, 75);
+    
+    for (int i = 0; i < sizeof(outputReportBuf); i++) {
+        outputReportBuf[i] = outputReportBuf[i+1];
+    }
+    
+    outputReportBuf[74] = crc32 & 0xFF;
+    outputReportBuf[75] = crc32 >> 8 & 0xFF;
+    outputReportBuf[76] = crc32 >> 16 & 0xFF;
+    outputReportBuf[77] = crc32 >> 24;
+    
+    hid_write(DS4Controller, outputReportBuf, 78);
+}
+
+void printInputReport(hid_device *DS4Controller) {
+    unsigned char inputReportBuf[10];
+    hid_read(DS4Controller, inputReportBuf, 10);
+    printf("Left Stick: (%d, %d)\n", inputReportBuf[1], inputReportBuf[2]);
+    printf("Right Stick: (%d, %d)\n", inputReportBuf[3], inputReportBuf[4]);
+    printf("DPad: %s\n", dpad[inputReportBuf[5] & 15]);
+    printf("Square: %d\n", (inputReportBuf[5] >> 4) & 1);
+    printf("X: %d\n", (inputReportBuf[5] >> 5) & 1);
+    printf("Circle: %d\n", (inputReportBuf[5] >> 6) & 1);
+    printf("Triangle: %d\n", (inputReportBuf[5] >> 7) & 1);
+    printf("L1: %d\n", inputReportBuf[6] & 1);
+    printf("Left Trigger: %d%%\n", (inputReportBuf[8]*100)/255);
+    printf("R1: %d\n", (inputReportBuf[6] >> 1) & 1);
+    printf("Right Trigger: %d%%\n", (inputReportBuf[9]*100)/255);
+    printf("Share: %d\n", (inputReportBuf[6] >> 4) & 1);
+    printf("Options: %d\n", (inputReportBuf[6] >> 5) & 1);
+    printf("L3: %d\n", (inputReportBuf[6] >> 6) & 1);
+    printf("R3: %d\n", (inputReportBuf[6] >> 7) & 1);
+    printf("PS: %d\n", inputReportBuf[7] & 1);
+    printf("TPAD: %d\n", (inputReportBuf[7] >> 1) & 1);
 }
 
